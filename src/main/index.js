979fe4fd96abe88c6af281cca1a0ede10d54c8eb
gggills4235 <gggills4235@gmail.com>
2023-01-17 17:54:23 +0900

    antd 기본 적용

--- src/main/index.js
+++ src/main/index.js
@@ -20,21 +20,16 @@ function MainPage() {
   }, []);
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map &&
+          products.map(function (product, index) {
             return (
               <div className="product-card">
-                <Link className="product-link" to={`/products/${index}`}>
+                <Link className="product-link" to={`/products/${product.id}`}>
                   <div>
                     <img className="product-img" src={product.imageUrl} />
                   </div>
@@ -53,9 +48,7 @@ function MainPage() {
               </div>
             );
           })}
-        </div>
       </div>
-      <div id="footer"></div>;
     </div>
   );
 }
