689c6f4762c5fdbb378b06839e5cb337f057aaf7
gggills4235 <gggills4235@gmail.com>
2023-01-20 23:17:39 +0900

    0120

--- src/main/index.js
+++ src/main/index.js
@@ -7,9 +7,7 @@ function MainPage() {
   const [products, setProducts] = React.useState([]);
   React.useEffect(function () {
     axios
-      .get(
-        "https://6c208ddd-5d9e-4527-806e-809e6426e573.mock.pstmn.io/products"
-      )
+      .get("http://localhost:8080/products")
       .then(function (result) {
         const products = result.data.products;
         setProducts(products);
